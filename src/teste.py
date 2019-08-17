9a8cc41d4c23ee13dcc1fd5e20ae6b656de8f54a
joaopcnogueira <joaonogueira@fisica.ufc.br>
2019-08-17 18:18:41 -0300

    interagindo com a máquina local

--- src/teste.py
+++ src/teste.py
@@ -4,8 +4,15 @@ print("numpy has been imported ...")
 import pandas as pd
 print("pandas has been imported ...")
 
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-print("SparkSession has been initiated!")
+#from pyspark.sql import SparkSession
+#spark = SparkSession.builder.getOrCreate()
+#print("SparkSession has been initiated!")
 
-print(f"Hello Docker!")
+
+df = pd.read_csv("/usr/src/app/input/titanic.csv", sep="\t")
+print(df.head())
+
+small_df = df.head()
+small_df.to_csv("/usr/src/app/output/small_titanic.csv", index=False, sep=';')
+
+print(f"\nHello Docker!")
